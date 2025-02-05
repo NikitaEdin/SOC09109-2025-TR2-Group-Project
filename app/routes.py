e4f0a06e0b4f9affaa51c644b89f51fd7ee5b0f1
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-02-05 23:01:43 +0000

    Added informational pages with Napier styling

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,18 @@ from app.models import User, Role
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', title='Home')
+
+
+########## INFORMATIONAL PAGES ###########
+@app.route("/privacy-policy")
+def privacy():
+    return render_template('/info/privacy_policy.html', title='Privacy Policy') 
+
+@app.route("/terms-of-service")
+def terms_of_service():
+    return render_template('/info/terms_of_service.html', title='Terms of Service') 
+
+@app.route("/contact-us")
+def contact_us():
+    return render_template('/info/contact_us.html', title='Contact') 
