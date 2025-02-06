3f89f173c5be62bf588272df9065d2569ebd6643
jimbo8927 <40646650@live.napier.ac.uk>
2025-02-06 10:50:19 +0000

    Added viabilityStudy.html in templates\forms Added viabilityStudyForm.py in forms Updated routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from app import app
-from flask import render_template
+from flask import render_template, request
+
+from app.forms.viabilityStudyForm import ViabilityStudyForm
 from app.models import User, Role
 
 
@@ -7,3 +9,20 @@ from app.models import User, Role
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+@app.route('/viability-study', methods=['GET', 'POST'])
+def viability_study():
+    form = ViabilityStudyForm()
+
+    if request.method == "POST":
+        # saves changes only for finalising later
+        if form.saveChanges.data:
+            print("Form Saved")
+
+        # submits completed form
+        if form.validate_on_submit() and form.submit.data:
+            print("Form Submitted")
+
+    return render_template("forms/viabilityStudy.html", form=form)
+
+
