6860aebf6da45ca18f21b6471d6a71909c191ddb
jimbo8927 <40646650@live.napier.ac.uk>
2025-02-08 14:37:13 +0000

    Added - 3 drone images
    Updated -
    routes.py
    getting_started.html
    viability_study.html
    view_drones.html

--- app/routes.py
+++ app/routes.py
@@ -23,4 +23,43 @@ def terms_of_service():
 def contact_us():
     return render_template('/info/contact_us.html', title='Contact')
 
+@app.route("/view-drones")
+def view_drones():
+
+    drones = {
+        '1': {
+            'name': 'DJI Matrice 350 RTK',
+            'image_path': 'Images/Matrice350RTK.jpg'
+        },
+        '2': {
+            'name': 'DJI Mavic 3E',
+            'image_path': '/Images/coenkpz4.png'
+        },
+        '3': {
+            'name': 'DJI Mini 3 Pro',
+            'image_path': '/Images/Mini3Pro.jpeg'
+        }
+    }
+
+    """
+        '4': {
+            'name': 'Drone 4',
+            'image_path': '/Images/DroneCityEdited.jpg'
+        },
+        '5': {
+            'name': 'Drone 5',
+            'image_path': '/Images/DroneCityEdited.jpg'
+        },
+        '6': {
+            'name': 'Drone 6',
+            'image_path': '/Images/DroneCityEdited.jpg'
+        },
+        '7': {
+            'name': 'Drone 7',
+            'image_path': '/Images/DroneCityEdited.jpg'
+        }
+    """
+
+    return render_template('/info/view_drones.html', drones=drones)
+
 
