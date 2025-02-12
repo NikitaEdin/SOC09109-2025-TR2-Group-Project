08bea69fc3a7092ead12a83ab0772358006bc400
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-02-12 16:47:22 +0000

    created optional forms
    Section to store the optional checks and forms

--- app/routes_forms.py
+++ app/routes_forms.py
@@ -16,3 +16,61 @@ def viability_study():
             print("Form Submitted")
 
     return render_template("/forms/viability_study.html", form=form)
+
+@app.route('/forms/optional', methods =['GET','POST'])
+def optional_forms_checks():
+    checks={
+        'leaflet_drop': {
+        'title': 'Leaflet Drop',
+        'description': 'Carry out a leaflet drop and/or a door-to-door advisory campaign if the flight operation is to take place in a highly populated area, such as a housing estate',
+        'value': False
+    },
+    'inform_police': {
+        'title': 'Inform Police',
+        'description': 'Inform the local police if the planned flight operation is to take place in areas where there is likely to be members of the public',
+        'value': False
+    },
+    'inform_local_air_user': {
+        'title': 'Inform Local Air Users',
+        'description': 'If there is a local air user club nearby, contact the club and enquire about any likely activity on the day of the proposed flight operation',
+        'value': False
+    },
+    'monitor_weather': {
+        'title': 'Monitor Weather',
+        'description': 'Monitor the weather conditions on the day of the proposed flight operation',
+        'value': False
+    },
+    'customise_loading_list': {
+        'title': 'Customise Loading List',
+        'description': 'Customise the loading list to suit the flight operation',
+        'value': False
+    },
+    'land_permission': {
+        'title': 'Permission from Land Owner',
+        'description': 'Obtain permission to land on the landowner’s property',
+        'value': False
+    },
+    'crew_call_sheets':{
+        'title': 'Prepare and send Crew Call Sheets',
+        'description' : 'Items for crew to bring on flight',
+        'value': False
+        
+    },
+    'post_flight_check':{
+        'title': 'Post Flight Form',
+        'description':'Form for post flight data',
+        'value': False
+    },
+    'pre_flight_check':{
+        'title':'Pre flight form',
+        'description': 'Form for pre flight data',
+        'value':False
+    },
+    'notam_form':{
+        'title':'Notam Form',
+        'description': 'Enter details into form',
+        'value':False
+    }
+    
+    }
+    return render_template("/forms/optional_forms.html", checks=checks)
