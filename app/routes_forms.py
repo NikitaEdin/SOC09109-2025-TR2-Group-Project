0c2e64f4b87548abe946c9f322a6168a478459ad
Ramsay <therealfloffah@gmail.com>
2025-02-14 10:55:56 +0000

    feat(forms): add crew call sheet form template (#2)

--- app/routes_forms.py
+++ app/routes_forms.py
@@ -1,5 +1,7 @@
 from app import app
 from flask import render_template, request
+
+from app.forms.crewCallSheetForm import CrewCallSheetForm
 from app.forms.viabilityStudyForm import ViabilityStudyForm
 
 @app.route('/forms/viability-study', methods=['GET', 'POST'])
@@ -16,3 +18,17 @@ def viability_study():
             print("Form Submitted")
 
     return render_template("/forms/viability_study.html", form=form)
+
+@app.route('/forms/crew_call_sheet', methods=['GET', 'POST'])
+def crew_call_sheet():
+    form = CrewCallSheetForm()
+
+    # save & submit handler
+    if request.method == "POST":
+        if form.saveChanges.data:
+            print("Form Saved")
+
+        if form.submit.data and form.validate_on_submit():
+            print("Form Submitted")
+
+    return render_template("/forms/crew_call_sheet.html", form=form)
