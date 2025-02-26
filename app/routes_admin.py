7a5edb8f07ee79f48ae2d467848b417fdfc211ce
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-02-26 21:35:00 +0000

    Add admin decorator and updated navigation

--- app/routes_admin.py
+++ app/routes_admin.py
@@ -1,9 +1,11 @@
 from flask import render_template, redirect, url_for, flash
 from app import app, db
 from flask_login import current_user
+from app.decorators import admin_required
 from app.models import User, Project
 
 @app.route("/admin/dashboard")
+@admin_required
 def admin_dashboard():
     if not current_user.is_authenticated or not current_user.is_admin():
         flash('You must be logged in as an admin to access this page.', 'danger')
@@ -13,6 +15,7 @@ def admin_dashboard():
     return render_template("admin_panel/admin_dashboard.html", num_users=num_users, num_projects=num_projects)
 
 @app.route("/admin/users")
+@admin_required
 def view_users():
     if not current_user.is_authenticated or not current_user.is_admin():
         flash('You must be logged in as an admin to access this page.', 'danger')
