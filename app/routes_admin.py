4ecdaa8cb67f313f87ae8e3ed4aad483560a4da4
AaronB233 <123958014+AaronB233@users.noreply.github.com>
2025-02-26 19:55:11 +0000

    add admin authentication and view users page
    Added authentication to restrict admin panel access to admin users only.  Added a new route and template to view all users and their information.

--- app/routes_admin.py
+++ app/routes_admin.py
@@ -1,9 +1,22 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app, db
+from flask_login import current_user
 from app.models import User
 
 @app.route("/admin/dashboard")
 def admin_dashboard():
+    if not current_user.is_authenticated or not current_user.is_admin():
+        flash('You must be logged in as an admin to access this page.', 'danger')
+        return redirect(url_for("login"))
     num_users = User.query.count()
     num_projects = 5
-    return render_template("admin_dashboard.html", num_users=num_users, num_projects=num_projects)
+    return render_template("admin_panel/admin_dashboard.html", num_users=num_users, num_projects=num_projects)
+
+@app.route("/admin/users")
+def view_users():
+    if not current_user.is_authenticated or not current_user.is_admin():
+        flash('You must be logged in as an admin to access this page.', 'danger')
+        return redirect(url_for("login"))
+    users = User.query.all()
+    return render_template("admin_panel/view_users.html", users=users)
+
