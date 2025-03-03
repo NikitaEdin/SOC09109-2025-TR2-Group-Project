0694c8261ba7035ddb13cca5c6e8dcb6ed21b413
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-03-03 12:39:29 +0000

    Admin panel template

--- app/routes_admin.py
+++ app/routes_admin.py
@@ -1,25 +1,42 @@
 from flask import render_template, redirect, url_for, flash
+from sqlalchemy import func
 from app import app, db
-from flask_login import current_user
 from app.decorators import admin_required
-from app.models import User, Project
+from app.models import AuditLog, Drone, User, Project
 
 @app.route("/admin/dashboard")
 @admin_required
 def admin_dashboard():
-    if not current_user.is_authenticated or not current_user.is_admin():
-        flash('You must be logged in as an admin to access this page.', 'danger')
-        return redirect(url_for("login"))
+    
     num_users = User.query.count()
     num_projects = Project.query.count()
-    return render_template("admin_panel/admin_dashboard.html", num_users=num_users, num_projects=num_projects)
+    num_logs = AuditLog.query.count()
+    num_drones = Drone.query.count()
+
+
+    # Projects by type
+    projects_by_type = dict(db.session.query(Project.projectType, func.count(Project.id)).group_by(Project.projectType).all())
+
+    # Recent projects
+    recent_projects = Project.query.order_by(Project.created_at.desc()).limit(5).all()
+
+    return render_template(
+        "admin_panel/admin_dashboard.html",
+        num_users=num_users,
+        num_projects=num_projects,
+        num_logs=num_logs,
+        projects_by_type=projects_by_type,
+        recent_projects=recent_projects,
+        num_drones=num_drones,
+       
+        title='admin panel',
+        use_container=False
+    )
 
 @app.route("/admin/users")
 @admin_required
 def view_users():
-    if not current_user.is_authenticated or not current_user.is_admin():
-        flash('You must be logged in as an admin to access this page.', 'danger')
-        return redirect(url_for("login"))
     users = User.query.all()
-    return render_template("admin_panel/view_users.html", users=users)
+    return render_template("admin_panel/view_users.html", users=users,
+                            title='users',)
 
