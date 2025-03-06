fb0949f1ead8fdfc25df399b33e171f2000b32ff
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-03-06 14:35:07 +0000

    Some JSONS created
    The ones that require a checklist, the others i am still thinking about how to tackle them.

---
+++ app/forms/jsons/loadingListEquipment.py
@@ -0,0 +1,41 @@
+LoadingListEquipment = [
+{
+  "title": "Loading List - Equipment",
+  "header": "Check that all equipment required is packed.",
+  "form": {
+    "sections": [
+      {
+        "header": "Camera Equipment",
+        "fields": [
+          {"id": "camerabolt", "name": "Camera Attachment Bolt", "value": "", "type": "checkbox", "required": True},
+          {"id": "cameralanyard", "name": "Camera to Airframe Lanyard", "value": "", "type": "checkbox", "required": True},
+          {"id": "cameraleads", "name": "Camera Connection Leads", "value": "", "type": "checkbox", "required": True},
+          {"id": "cameralens", "name": "Camera(s) & Lens(s)", "value": "", "type": "checkbox", "required": True},
+          {"id": "cameramount", "name": "Camera Mount", "value": "", "type": "checkbox", "required": True},
+          {"id": "camerabattery", "name": "Camera Battery Packs", "value": "", "type": "checkbox", "required": True}
+        ]
+      },
+      {
+        "header": "Batteries & Chargers",
+        "fields": [
+          {"id": "chargerlead", "name": "Required Charger Leads", "value": "", "type": "checkbox", "required": True},
+          {"id": "batterycharger", "name": "Multi-Function Battery Charger", "value": "", "type": "checkbox", "required": True},
+          {"id": "chargerbatterpack", "name": "Charger Battery Packs", "value": "", "type": "checkbox", "required": True},
+          {"id": "monitorbatterypack", "name": "Monitor Battery Packs", "value": "", "type": "checkbox", "required": True},
+          {"id": "transmitterbatterypack", "name": "Transmitter Battery Packs", "value": "", "type": "checkbox", "required": True}
+        ]
+      },
+      {
+        "header": "Other Equipment",
+        "fields": [
+          {"id": "reportincidents", "name": "Food (Snacks & Drink)", "value": "", "type": "checkbox", "required": True},
+          {"id": "reportairprox", "name": "Two Way Radios", "value": "", "type": "checkbox", "required": True},
+          {"id": "reportairprox", "name": "Air Navigation Map", "value": "", "type": "checkbox", "required": True},
+          {"id": "reportairprox", "name": "Checklists, Manuals & Logbooks", "value": "", "type": "checkbox", "required": True},
+          {"id": "reportairprox", "name": "Notepad & Pens", "value": "", "type": "checkbox", "required": True}  
+        ]
+      }
+    ]
+  }
+}
+]
---
+++ app/forms/jsons/loadingListMaintenanceKit.py
@@ -0,0 +1,20 @@
+LoadingListMaintenanceKit = [
+{
+  "title": "Loading List - Maintenance Kit",
+  "header": "Ensure all maintenance kit items are checked before proceeding.",
+  "form": {
+    "sections": [
+      {
+        "header": "Maintenance Equipment",
+        "fields": [
+          {"id": "props", "name": "Spare props", "value": "", "type": "checkbox", "required": True},
+          {"id": "cables", "name": "Spare cables", "value": "", "type": "checkbox", "required": True},
+          {"id": "allenkeys", "name": "Allen keys", "value": "", "type": "checkbox", "required": True},
+          {"id": "screwdrivers", "name": "Screwdrivers", "value": "", "type": "checkbox", "required": True},
+          {"id": "calibration", "name": "Calibration platform", "value": "", "type": "checkbox", "required": True}
+        ]
+      }
+    ]
+  }
+}
+]
