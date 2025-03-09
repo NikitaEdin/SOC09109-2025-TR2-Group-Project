8675df8d82ca7178365d60d0c6a7c313801b2d26
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-03-09 13:37:46 +0000

    Json templates created

---
+++ app/forms/jsons/loadingListCrewTemplate.py
@@ -0,0 +1,43 @@
+LoadingListCrewTemplate = [
+{
+  "title": "Loading List - Crew",
+  "header": "Enter all of the crew and their details.",
+  "form": {
+    "sections": [
+      {
+        "header": "Crew Name",
+        "fields": [
+          {"id": "crewid1", "name": "Name", "value": "", "type": "text", "required": True}
+
+        ]
+      },
+      {
+        "header": "Role",
+        "fields": [
+          {"id": "crewid1", "name": "Role", "value": "", "type": "text", "required": True}
+        ]
+      },
+      {
+        "header": "Contact Number",
+        "fields": [
+          {"id": "crewid1", "name": "Contact Number", "value": "", "type": "text", "required": True}
+        ]
+      },
+        {
+        "header": "Email",
+        "fields": [
+          {"id": "crewid1", "name": "Email", "value": "", "type": "text", "required": True}
+ 
+        ]
+      },
+          {
+        "header": "Called",
+        "fields": [
+          {"id": "crewid1", "name": "Called", "value": "", "type": "checkbox", "required": True}
+
+        ]
+      }
+    ]
+  }
+}
+]
---
+++ app/forms/jsons/loadingListGroundEquipmentTemplate.py
@@ -0,0 +1,29 @@
+LoadingListGroundEquipmentTemplate = [
+{
+  "title": "Loading List - Ground Equipment",
+  "header": "Enter all of ground equipment you will be bringing.",
+  "form": {
+    "sections": [
+      {
+        "header": "Item",
+        "fields": [
+          {"id": "groundid1", "name": "Item", "value": "", "type": "text", "required": True}
+
+        ]
+      },
+      {
+        "header": "Action",
+        "fields": [
+          {"id": "groundid1", "name": "Action", "value": "", "type": "text", "required": True}
+        ]
+      },
+      {
+        "header": "Check",
+        "fields": [
+          {"id": "groundid1", "name": "Check", "value": "", "type": "checkbox", "required": True}
+        ]
+      }
+    ]
+  }
+}
+]
