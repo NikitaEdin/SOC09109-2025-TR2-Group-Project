329e9a981ebf3fc48fab441c5ad4ce2dc74a99fa
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-03-14 14:05:31 +0000

    Update loadingListCrewListTemplate.py
    Testing

--- app/forms/jsons/loadingListCrewListTemplate.py
+++ app/forms/jsons/loadingListCrewListTemplate.py
@@ -7,33 +7,54 @@ LoadingListCrewListTemplate = [
       {
         "header": "Crew Name",
         "fields": [
-          {"id": "name1", "name": "", "value": "", "type": "text", "required": True}
+          {"id": "name1", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "name2", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "name3", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "name4", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "name5", "name": "", "value": "", "type": "text", "required": True}
 
         ]
       },
       {
         "header": "Role",
         "fields": [
-          {"id": "role1", "name": "", "value": "", "type": "text", "required": True}
+          {"id": "role1", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "role2", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "role3", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "role4", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "role5", "name": "", "value": "", "type": "text", "required": True}
         ]
       },
       {
         "header": "Contact Number",
         "fields": [
-          {"id": "num1", "name": "", "value": "", "type": "text", "required": True}
+          {"id": "num1", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "num2", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "num3", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "num4", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "num5", "name": "", "value": "", "type": "text", "required": True}
         ]
       },
         {
         "header": "Email",
         "fields": [
-          {"id": "email1", "name": "", "value": "", "type": "text", "required": True}
+          {"id": "email1", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "email2", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "email3", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "email4", "name": "", "value": "", "type": "text", "required": True},
+          {"id": "email5", "name": "", "value": "", "type": "text", "required": True}
+          
  
         ]
       },
           {
         "header": "Called",
         "fields": [
-          {"id": "check1", "name": "", "value": "", "type": "checkbox", "required": True}
+          {"id": "check1", "name": "", "value": "", "type": "checkbox", "required": True},
+          {"id": "check2", "name": "", "value": "", "type": "checkbox", "required": True},
+          {"id": "check3", "name": "", "value": "", "type": "checkbox", "required": True},
+          {"id": "check4", "name": "", "value": "", "type": "checkbox", "required": True},
+          {"id": "check5", "name": "", "value": "", "type": "checkbox", "required": True}
 
         ]
       }
