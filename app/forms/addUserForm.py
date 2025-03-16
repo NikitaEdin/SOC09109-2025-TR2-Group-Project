0ec16e4628abd2fd033b8306d6c325e8549fe3ea
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-03-16 21:16:04 +0000

    Add error messages and password validator

--- app/forms/addUserForm.py
+++ app/forms/addUserForm.py
@@ -1,12 +1,16 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SelectField, SubmitField
+from wtforms import StringField, PasswordField, SelectField, SubmitField, validators
 from wtforms.validators import DataRequired, Email, Length, EqualTo
 from app.models import Role
+from app.utils.validators import validate_password
 
 class addUserForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired(), Length(min=3, max=18)])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    password = PasswordField("Password", validators=[
+        DataRequired(), 
+        validate_password,  
+        validators.EqualTo('confirm_password', message='Passwords must match')])
     confirm_password = PasswordField("Confirm Password", validators=[DataRequired(), EqualTo('password')])
     role_id = SelectField("Role", coerce=int, validators=[DataRequired()])
     submit = SubmitField("Add User")
