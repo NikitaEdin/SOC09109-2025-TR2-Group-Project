08e3eb26bff822d2465059b9069211a8824a7bee
NikitaEdin <nikita.lanetsky.uk@gmail.com>
2025-03-25 11:17:58 +0000

    Add static timeline
    Implemented a static timeline accessible via the dashboard menu, and the items are populated from a JSON file for easier maintenance.

---
+++ app/forms/jsons/static_timeline.py
@@ -0,0 +1,167 @@
+Timeline_data = [
+{
+  "sections": [
+    {
+      "title": "28 Days in Advance",
+      "items": [
+        {
+          "text": "Apply for non-standard flight permission if required",
+          "url": "https://nsf.nats.aero/drones-and-model-aircraft/"
+        }
+      ]
+    },
+    {
+      "title": "7 Days in Advance",
+      "items": [
+        {
+          "text": "Complete viability study"
+        },
+        {
+          "text": "Complete site evaluation"
+        },
+        {
+          "text": "Complete risk analysis"
+        },
+        {
+          "text": "Submit NOTAM if appropriate",
+          "url": "https://www.caa.co.uk/consumers/remotely-piloted-aircraft/our-role/airspace-notification-of-uas/"
+        },
+        {
+          "text": "Carry out a leaflet drop and/or a door-to-door advisory campaign if the flight operation is in a highly populated area"
+        },
+        {
+          "text": "Inform the local police if the flight operation is in public areas"
+        },
+        {
+          "text": "Contact local air user clubs about potential activity"
+        },
+        {
+          "text": "Obtain written permission from relevant landowners/occupiers"
+        },
+        {
+          "text": "Monitor the weather at the site"
+        },
+        {
+          "text": "Customise the loading list"
+        }
+      ]
+    },
+    {
+      "title": "24 Hours Before Flight",
+      "items": [
+        {
+          "text": "Contact ATC if operating in a flight restriction zone"
+        },
+        {
+          "text": "Make go/no-go decision based on weather"
+        },
+        {
+          "text": "Review the loading list"
+        },
+        {
+          "text": "Prepare and send crew call sheets",
+          "url": "https://bdavison.napier.ac.uk/opsman/documents/Call_sheet_V0.1.docx"
+        },
+        {
+          "text": "Configure UAS as needed"
+        },
+        {
+          "text": "Update UAS firmware"
+        },
+        {
+          "text": "Conduct a test flight if necessary"
+        },
+        {
+          "text": "Ensure operator ID is clearly displayed"
+        },
+        {
+          "text": "Charge all flight batteries, controllers, and devices"
+        },
+        {
+            "text": "Customise the site checklist",
+            "url": "https://bdavison.napier.ac.uk/opsman/documents/Site_checklist_V0.1.docx"
+        },
+        {
+          "text": "Print required flight and UAS documentation"
+        },
+        {
+            "text": "- Permissions and contacts"
+        },
+        {
+            "text": "- Emergency procedures"
+        },
+        {
+            "text": "- Customised loading list"
+        },
+        {
+            "text": "- Customised site checklist"
+        },
+        {
+            "text": "- UAS assembly instructions"
+        },
+        {
+            "text": "- Crew briefing notes",
+            "url": "https://bdavison.napier.ac.uk/opsman/documents/Crew_briefing_V0.1.docx"
+        },
+        {
+            "text": "Pre-flight checklist",
+            "url": "https://bdavison.napier.ac.uk/opsman/documents/Pre_flight_checklist_V0.1.docx"
+        },
+        {
+            "text": "Post-flight actions checklist",
+            "url": "https://bdavison.napier.ac.uk/opsman/documents/Post_flight_actions_V0.1.docx"
+        }
+      ]
+    },
+    {
+      "title": "Day of Flight",
+      "items": [
+        {
+          "text": "Ensure relevant operator account is logged in"
+        },
+        {
+          "text": "Pack equipment using the loading list"
+        },
+        {
+          "text": "Check for NOTAMs",
+          "url": "https://notaminfo.com/nationalmap"
+        },
+        {
+          "text": "Complete site checklist (including a physical tour of the area)"
+        },
+        {
+          "text": "Make go/no-go decision for the flight based on windspeed, weather, presence of uninvolved persons and any unexpected factors"
+        },
+        {
+          "text": "Assemble the UAS using model-specific checklist"
+        },
+        {
+          "text": "Complete crew briefing"
+        },
+        {
+          "text": "Complete pre-flight checks"
+        },
+        {
+          "text": "Complete post-flight actions"
+        },
+        {
+            "text": "Disassemble the UAS using model-specific checklist and pack equipment"
+        }
+      ]
+    },
+    {
+      "title": "Incidents & Emergencies",
+      "items": [
+        {
+          "text": "Report an incident through ECCAIRS",
+          "url": "https://aviationreporting.eu/"
+        },
+        {
+          "text": "Report an AIRPROX",
+          "url": "https://www.airproxboard.org.uk/home/"
+        }
+      ]
+    }
+  ]
+}
+]
