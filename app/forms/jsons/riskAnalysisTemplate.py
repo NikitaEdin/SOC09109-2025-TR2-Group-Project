32affbd0ab383dbc7b13ea5c1e89589dbb47c3b0
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2025-03-27 20:25:11 +0000

    Update riskAnalysisTemplate.py
    This is adding the missing information in the existing control and adding value field in preparation for backend

--- app/forms/jsons/riskAnalysisTemplate.py
+++ app/forms/jsons/riskAnalysisTemplate.py
@@ -7,27 +7,21 @@ riskAnalysisTemplate = [
       {
         "header": "Hazards Identified",
         "dropdown_hazards": [
-          {"id": "compliance", "name": "Compliance","required": True},
-          
-          {"id": "competence", "name": "Competence","required": True},
-          
-          {"id": "permission", "name": "Permission","required": True},
-          
-          {"id": "weather", "name": "Weather","required": True},
-          
-          {"id": "pederstrians_traffic", "name": "Pedestrians and Traffic","required": True},
-          
-          {"id": "emergency", "name": "Emergency","required": True},
-          
-          {"id": "wildlife_pets", "name": "Wildlife, Livestock, Domestic pets etc","required": True},
-          {"id": "collision_nearmiss", "name": "UAS collision or near miss with other air users","required": True},
-          {"id": "maintenance_servicing", "name": "Maintenance and servicing","required": True},
-          {"id": "crash_building", "name": "Crash into building/people","required": True},
-          {"id": "security", "name": "Security","required": True},
-          {"id": "insurance", "name": "Insurance","required": True},
-          {"id": "data_protection", "name": "Data Protection/camera","required": True},
-          {"id": "ground_hazards", "name": "Ground Hazards","required": True},
-          {"id": "other_hazards", "name": "Any other Hazards/risks","required": True},
+          {"id": "compliance", "name": "Compliance", "value":"","required": True},
+          {"id": "competence", "name": "Competence", "value":"","required": True},
+          {"id": "permission", "name": "Permission", "value":"","required": True},
+          {"id": "weather", "name": "Weather","value":"","required": True},
+          {"id": "pederstrians_traffic", "name": "Pedestrians and Traffic","value":"","required": True},
+          {"id": "emergency", "name": "Emergency","value":"","required": True},
+          {"id": "wildlife_pets", "name": "Wildlife, Livestock, Domestic pets etc","value":"","required": True},
+          {"id": "collision_nearmiss", "name": "UAS collision or near miss with other air users","value":"","required": True},
+          {"id": "maintenance_servicing", "name": "Maintenance and servicing","value":"","required": True},
+          {"id": "crash_building", "name": "Crash into building/people","value":"","required": True},
+          {"id": "security", "name": "Security","value":"","required": True},
+          {"id": "insurance", "name": "Insurance","value":"","required": True},
+          {"id": "data_protection", "name": "Data Protection/camera","value":"","required": True},
+          {"id": "ground_hazards", "name": "Ground Hazards","value":"","required": True},
+          {"id": "other_hazards", "name": "Any other Hazards/risks","value":"","required": True},
         ]
       },
       {
@@ -42,16 +36,52 @@ riskAnalysisTemplate = [
           {"id": "permission_control", "name":
             """The pilot will seek permission in advance in line with the University procedure.\n Off campus, permission to fly is required from the property owner.\n Where required notification to air traffic control.\n Approval of location and risk assessment to area must be completed."""},
           
+          {"id": "weather_control", "name":
+            """Accurate weather forecast checked prior to flight.\n Weather to be monitored at all times during flight with a view to landing should weather deteriorate."""},
+           
+          {"id": "pederstrians_traffic_control", "name":
+            """Tape off exclusion zone where possible.\n At least one spotter is dedicated to supervising access to zone.\n Flying in the middle of the field and away from pedestrians and traffic."""},
+           
+          {"id": "emergency_control", "name":
+            """Local emergency arrangements in place.\n Campus induction leaflet communicated."""},
+           
+          {"id": "wildlife_pets_control", "name":
+            """Risk assess area prior to flight of risks to livestock or restrictions.\n Keep a responsible distance away from domestic animals, pets and working animals (e.g. assistance dogs)."""},
+           
+          {"id": "collision_nearmiss_control", "name":
+            """Activity in line with CAA legislative requirements and Operations Manual.\n Air Traffic Control will be informed. """},
+           
+          {"id": "maintenance_servicing_control", "name":
+            """The drone/UAV will be serviced and maintained in line with the manufacturers instructions.\n Batteries will be stored within a fireproof bag whilst they are charging. """},
+           
+          {"id": "crash_building_control", "name":
+            """The remote pilot has undertaken relevant training.\n The Pilot must ensure they are 50 meters away from people and property."""},
+           
+          {"id": "security_control", "name":
+            """No lone working when flying drone/UAV permitted.\n Staff will not leave any property unattended."""},
+           
+          {"id": "insurance_control", "name":
+            """The Universitys public liability and employers liability insurances would respond where damage and/or injury to 3rd party property, assets or individuals was caused by flight of a drone/UAV and the University was found to be negligent.\n Insurance stipulates NQE training required."""},
+          
+          {"id": "data_protection_control", "name":
+            """Consideration must be given in relation to images gathered.\n This must be taken into account when considering the flight location and should be stated in the site-specific risk assessment if applicable."""},
+          
+          {"id": "ground_hazards_control", "name":
+            """e.g. Trees, unstable terrain, buildings, water, etc."""},
+          
+          {"id": "other_hazards_control", "name":
+            """Anything not listed."""},
+          
         ]
       },
       {
         "header": "People at Risk",
         "dropdown_people":[
-            {"id": "all", "name": "All in vicinity","required": True},
-            {"id": "staff", "name": "Staff in vicinity","required": True},
-            {"id": "students", "name": "Students in vicinity","required": True},
-            {"id": "visitors", "name": "Visitors in vicinity","required": True},
-            {"id": "pedestrians", "name": "Pedestrians","required": True},
+            {"id": "all", "name": "All in vicinity","value":"","required": True},
+            {"id": "staff", "name": "Staff in vicinity","value":"","required": True},
+            {"id": "students", "name": "Students in vicinity","value":"","required": True},
+            {"id": "visitors", "name": "Visitors in vicinity","value":"","required": True},
+            {"id": "pedestrians", "name": "Pedestrians","value":"","required": True},
         ]
       },
       {
@@ -59,7 +89,6 @@ riskAnalysisTemplate = [
       },
       {
         "header": "Further Actions"
-        
       },
       {
         "header": "Residual Risk"
